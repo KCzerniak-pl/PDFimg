671e6624c21e424e79ae09bc7e0b0362a1f89dca
KCzerniak-pl <kczerniak@outlook.com>
2023-03-15 09:04:49 +0100

    Created MVVM structure. Added main window.

---
+++ PDFimg/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using PDFimg.Views;
+using Prism.Commands;
+using Prism.Mvvm;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PDFimg.ViewModels
+{
+    class MainWindowViewModel : BindableBase
+    {
+        // Button for minimize app.
+        private ICommand? _minimizeAppCommand;
+        public ICommand MinimizeAppCommand { get => _minimizeAppCommand ?? (_minimizeAppCommand = new DelegateCommand(() => SystemCommands.MinimizeWindow(Application.Current.MainWindow))); }
+
+        // Button for close app.
+        private ICommand? _closeAppCommand;
+        public ICommand CloseAppCommand { get => _closeAppCommand ?? (_closeAppCommand = new DelegateCommand(() => Application.Current.Shutdown())); }
+
+        // Active view.
+        private object _currentView = null!;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { SetProperty(ref _currentView, value); }
+        }
+
+        // Marker for selected option in the menu.
+        private Thickness _slidebarMarkMargin;
+        public Thickness SlidebarMarkMargin
+        {
+            get { return _slidebarMarkMargin; }
+            set { SetProperty(ref _slidebarMarkMargin, value); }
+        }
+
+        // Selecting an item in the menu.
+        public int SidebarSelectedIndex
+        {
+            set
+            {
+                SlidebarMarkMargin = new Thickness(0, 65 * value, 0, 0);
+            }
+        }
+    }
+}
---
+++ PDFimg/Views/MainWindowView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace PDFimg.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindowView.xaml
+    /// </summary>
+    public partial class MainWindowView : Window
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+        }
+
+        private void HeaderLoaded(object sender, RoutedEventArgs e)
+        {
+            TitleBar.MouseLeftButtonDown += (s, e) =>
+            {
+                DragMove();
+            };
+        }
+    }
+}
